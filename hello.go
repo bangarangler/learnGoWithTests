a215c9be7a9de31b1669f53a1b405523503a1263
Jon <bangarangler@gmail.com>
2021-06-16 11:51:43 -0400

    added support for spanish and french getting ready to refactor

--- hello.go
+++ hello.go
@@ -2,15 +2,29 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	englishHelloPrefix = "Hello, "
+	spanish            = "Spanish"
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	if language == "Spanish" {
+		return spanishHelloPrefix + name
+	}
+
+	if language == "French" {
+		return frenchHelloPrefix + name
+	}
+
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
