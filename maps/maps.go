de6d636895404ef70a6bc92d2a09492474c114bf
Jon <bangarangler@gmail.com>
2021-06-18 08:37:46 -0400

    added maps package and full crud plus tests

--- maps/maps.go
+++ maps/maps.go
@@ -1,12 +1,37 @@
 package maps
 
-import "errors"
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
 
-type Dictionairy map[string]string
+type Dictionary map[string]string
+type DictionaryErr string
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
-func (d Dictionairy) Search(word string) (string, error) {
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
+
+func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 	if !ok {
 		return "", ErrNotFound
@@ -14,6 +39,15 @@ func (d Dictionairy) Search(word string) (string, error) {
 	return definition, nil
 }
 
-// func Search(dictionary map[string]string, word string) string {
-// 	return dictionary[word]
-// }
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
+}
